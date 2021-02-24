834b64abff24f742e85b9f33fd000e7ac859e0c8
Johnny <johnny.marlon.paula@gmail.com>
2021-02-24 19:28:20 -0300

    more lessons

--- SCRIPTS/functions.js
+++ SCRIPTS/functions.js
@@ -1,12 +1,56 @@
-// Criar um aplicativo de frases motivacionais
+// function expression
+// function anonymous
 
-// Function statement
-function createPhrases() {
-    console.log('Estudar é muito bom')
-    console.log('Paciência e persistência')
-    console.log('Revisão é a mãe do aprendizado')
+//parâmetros (parameters)
+/*const sum = function (number1, number2) {
+    let total = number1 + number2
+    return total
 }
 
-// Executar, rodar, chamar, invocar a função
-// Execute, run, call, invoke the function
-createPhrases()
+let number1 = 'a'
+let number2 = 'b'
+sum(number1, number2) // arguments = argumentos
+
+console.log(`o número 1 é: ${number1}`)
+console.log(`o número 2 é: ${number2}`)
+console.log(`a soma é: ${sum(number1, number2)}`)
+*/
+/*//Function hoisting
+
+sayMyName()
+
+function sayMyName() {
+    console.log('Johnny')
+}*/
+
+/*
+//Arrow function
+
+const sayMyName = (name) => {
+
+    console.log(name)
+}
+
+sayMyName('Johnny Marlon')
+*/
+
+//Callback function
+/*
+function sayMyName(name){
+
+    console.log('antes de executar a funcao callback')
+
+    name()
+
+    console.log('depois de executar a callback')
+}
+
+sayMyName(
+    () => {
+        console.log('estou em uma callback')
+    }
+)
+*/
+
+//Funções construtoras
+
---
+++ SCRIPTS/functions1.js
@@ -0,0 +1,19 @@
+// Criar um aplicativo de frases motivacionais
+
+// Function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+// Executar, rodar, chamar, invocar a função
+// Execute, run, call, invoke the function
+let count = 1
+while (count <= 100) {
+    console.log(`${count}ª vez`)
+    createPhrases()
+    count = count + 1
+}
+
+console.log('fim do programa')
