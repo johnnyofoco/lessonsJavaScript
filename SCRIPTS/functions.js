b4d196a5d8c9c73363e8d4cd8cfca312da1d35d2
Johnny <johnny.marlon.paula@gmail.com>
2021-02-25 21:06:21 -0300

    more lessons

--- SCRIPTS/functions.js
+++ SCRIPTS/functions.js
@@ -150,6 +150,7 @@ let word = "manipulação"
 console.log(Array.from(word))
 */
 
+/*
 //Manipulando Arrays
 
 let techs = ["html", "css", "js"]
@@ -172,3 +173,235 @@ techs.push("nodejs", "sql")
 console.log(techs)
 let index = techs.indexOf('nodejs')
 console.log(`O index de 'nodejs' no array é: ${index}`)
+*/
+
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+  let number = 1
+  Binary: console.log(1 + 1)
+  Unary: console.log(++number)
+  Ternary: console.log(true ? 'alo' : 'nada')
+
+*/
+
+/* New = expressão para criar um novo Objeto
+let name = new String()
+name.surName = "Brito"
+let age = new Number(23)
+console.log(name, name.surName, age)
+*/
+
+/* Operadores unários
+    typeof
+    delete
+
+    const person = {
+    name: 'Mayk',
+    age: 25,
+}
+
+delete person.age
+
+console.log(typeof person , person)
+*/
+
+/* Operadores Aritiméticos
+  multiplicação: * (2*4)=8
+  divisão: / (25/5)=5
+  soma: + (7+7)=14
+  subtração: - (10-1) = 9
+
+  resto da divisão: % (10 % 2) = 0
+  incremento: ++ (0++)=1
+  decremento: -- (12--)=11
+  exponencial: ** (3 ** 2)= 3x3 = 9
+*/
+
+/* Grouping operator ( ) = pode trocar a ordem das precedências através do uso de parenteses
+
+Ordem de precedência: (), *ou/ , +ou-
+
+let total = 2 + 3 * 5
+*/
+
+/* Operadores de comparação
+
+  == igual
+  != diferente
+  === estritamente igual
+  !== estritamente diferente
+  > maior
+  >= maior igual
+  < menor
+  <= menor igual
+
+  let one = 1
+  let two = 2
+
+  console.log(one === '1')
+
+*/
+
+/* Operadores de atribuição (assignment)
+
+// assignment
+ x = 1
+
+ // addition assignment
+ x = x + 2
+ x += 2
+
+// subtraction assignment
+x -= 1
+console.log(x)
+
+//multiplication assignment
+x = x * 2
+x *= 2
+
+//division assignment
+x = x/2
+x /= 2
+
+//remainder, exponetiation
+x %= 2
+x **= 2
+
+*/
+
+
+/* Operadores lógicos
+
+//AND &&
+//OR ||
+//NOT !
+
+*/
+/*
+// Operador Condicional (Ternário)
+
+// Dependendo da condição, nós receberemos valores diferentes
+
+//Ex: true && trye ? 'faça assim' : 'faça outra coisa'
+
+let pao = false
+let queijo = false
+
+const niceBrakFast = pao || queijo ? 'Café top' : 'Café ruim'
+
+console.log(niceBrakFast)
+*/
+
+//Operadores para String
+//Comparison (comparação): ==
+
+//Concatenation (concatenação)
+// Retorna a união de duas Strings
+//Ex:'String ' + 's' = 'Strings'
+
+/*
+  //Falsy   //Truthy
+    //Quando um valor é considerado false para Falsy e true para Truthy em contextos onde um booleano é obrigatório ( condicionais e loops)
+
+    false
+    0
+    -0
+    ""
+    nullundefined
+    Nan
+
+    ou
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+
+/*
+  //Precedência dos operadores
+
+* grouping                           ( )
+* negação e incremento               ! ++ --
+* multiplicação e divisão            * /
+* adição e subtração                 + -
+* relacional                         < <= > >=
+* igualdade                          == !== === !==
+* AND                                &&
+* OR                                 ||
+* condicional                        ?:
+* assignment (atribuição)            = += -= *=
+
+*/
+
+/* CONTROLE DE FLUXO
+
+ex:
+
+
+//IF
+let temperature = 36.9
+
+if (temperature >= 37.5) {
+    console.log('Febre alta')
+} else if (temperature < 37.5 && temperature >= 37) {
+    console.log('Febre moderada')
+}else {
+    console.log('Saudável')
+}
+
+// SWITCH
+
+function calculate(number1, operator, number2) {
+    let result = 0
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break
+        case '-':
+            result = number1 - number2
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            result = number1 / number2
+            break
+        default:
+            console.log('não implementado')
+            break
+    }
+
+    return result
+}
+
+console.log(calculate(500, '%', 353))
+
+*/
+
+
+//THROW
+
+function sayMyName(name = '') {
+    console.log(name)
+    if (name === '') {
+        throw new Error("Nome é necessário")
+    }
+    console.log('tudo funcionou bem')
+}
+
+//TRY...CATCH
+try {
+    sayMyName()
+} catch (e) {
+    console.log(e)
+}
